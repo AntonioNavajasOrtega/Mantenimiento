78c95a1ae7d1c53ef8d1cdf1764dca54d898c724
Antonio <antonionavajaortega@uma.es>
2023-02-09 19:47:54 +0100

    ejercicio1

--- Ejercicio1/src/main/java/org/example/Person.java
+++ Ejercicio1/src/main/java/org/example/Person.java
@@ -1,4 +1,52 @@
 package org.example;
 
+import java.util.List;
+
+// @author Antonio Navajas Ortega
 public class Person {
+
+    private final String name;
+    private final int age;
+    private final String gender;
+
+    public Person(String name,int age,String gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String name()
+    {
+        return name;
+    }
+    public int age(){
+        return age;
+    }
+    public String gender()
+    {
+        return gender;
+    }
+
+    public double[] averageAgePerGender(List<Person> persons)
+    {
+        double [] res = new double[2];
+        double sumMale = 0;
+        double sumFemale = 0;
+        double cont1 = 0;
+        double cont2 = 0;
+        for (Person p : persons) {
+            if (p.gender.equals("Male")) {
+                sumMale += p.age;
+                cont1++;
+            } else {
+                sumFemale += p.age;
+                cont2++;
+            }
+        }
+        res[0] = sumMale / cont1;
+        res[1] = sumFemale / cont2;
+        return res;
+    }
+
 }
--- Ejercicio1/src/test/java/PersonTest.java
+++ Ejercicio1/src/test/java/PersonTest.java
@@ -1,2 +1,41 @@
-public class PersonTest {
+
+import org.example.*;
+        import org.junit.jupiter.api.AfterEach;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+
+        import static org.junit.jupiter.api.Assertions.assertEquals;
+        import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class PersonTest {
+    Person person;
+    @BeforeEach
+    void setup(String name, int age, String gender){
+        assert name != null && name.length() <= 50;
+        assert age >= 0 && age <= 120;
+        assert gender.equals("Male") || gender.equals("Female");
+
+        person = new Person(name,age,gender);
+    }
+
+    @AfterEach
+    void shutdown(){
+        person = null;
+    }
+
+
+    @Test
+    void agePerGenderTest1(){
+
+
+
+    }
+
+
+
+
+
+
+
 }
