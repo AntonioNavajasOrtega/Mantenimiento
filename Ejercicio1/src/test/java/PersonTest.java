9f46cf80f3a623a21a928a6f46ae7d10db50c279
Antonio <antonionavajasortega@uma.es>
2023-02-14 09:23:50 +0100

    ejercicio2.0 gitignore

--- Ejercicio1/src/test/java/PersonTest.java
+++ Ejercicio1/src/test/java/PersonTest.java
@@ -4,19 +4,17 @@ import org.example.*;
         import org.junit.jupiter.api.BeforeEach;
         import org.junit.jupiter.api.Test;
 
-        import static org.junit.jupiter.api.Assertions.assertEquals;
-        import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class PersonTest {
     Person person;
     @BeforeEach
-    void setup(String name, int age, String gender){
-        assert name != null && name.length() <= 50;
-        assert age >= 0 && age <= 120;
-        assert gender.equals("Male") || gender.equals("Female");
-
-        person = new Person(name,age,gender);
+    void setup(){
+        person = new Person("Antonio",20,"Male");
     }
 
     @AfterEach
@@ -24,18 +22,70 @@ class PersonTest {
         person = null;
     }
 
+    @Test
+    void testName()
+    {
+        String expectedName = "Antonio";
+        assertEquals(expectedName,person.getName());
+    }
 
     @Test
-    void agePerGenderTest1(){
+    void testAge()
+    {
+        int expectedAge = 20;
+        assertEquals(expectedAge,person.getAge());
+    }
 
-
-
+    @Test
+    void testGender()
+    {
+        String expectedGender = "Male";
+        assertEquals(expectedGender,person.getGender());
     }
 
 
+    @Test
+    void agePerGenderTest1Male0Female()
+    {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Javier",30,"Male"));
+        double [] expectedRes = {30,-1};
+        assertArrayEquals(expectedRes,person.averageAgePerGender(persons));
 
+    }
 
+    @Test
+    void agePerGenderTest0Male1Female()
+    {
+            List<Person> persons = new ArrayList<>();
+            persons.add(new Person("Nerea",30,"Female"));
+            double [] expectedRes = {-1,30};
+        assertArrayEquals(expectedRes,person.averageAgePerGender(persons));
+    }
 
+    @Test
+    void agePerGenderTest0Male0Female()
+    {
+                List<Person> persons = new ArrayList<>();
+                double [] expectedRes = {-1,-1};
+        assertArrayEquals(expectedRes,person.averageAgePerGender(persons));
+    }
+
+    @Test
+    void agePerGenderTestMaleFemale()
+    {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Nerea",30,"Female"));
+        persons.add(new Person("Javier",30,"Male"));
+        persons.add(new Person("María",47,"Female"));
+        persons.add(new Person("Alejandro",28,"Male"));
+        persons.add(new Person("Adriana",18,"Female"));
+        persons.add(new Person("Carlos",52,"Male"));
+        persons.add(new Person("Marta",35,"Female"));
+        persons.add(new Person("Sergio",49,"Male"));
+        double [] expectedRes = {39.75,32.5};
+        assertArrayEquals(expectedRes,person.averageAgePerGender(persons));
+    }
 
 
 }
